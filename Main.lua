ed2fb0ff50c15d4228554b9ba059fde90da6f10d
GodoSop <wierdroblxaccs@gmail.com>
2025-05-16 14:37:32 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -51,7 +51,7 @@ local WinButton = MainTab:CreateButton({
       end
 
       repeat wait() until game.Players.LocalPlayer.PlayerGui.Game.Background.MainText.Text == "GO!"
-      finishPart.Position = Player.CFrame
+      finishPart.Position = Vector3.new(Player.CFrame)
    end,
 })
 
